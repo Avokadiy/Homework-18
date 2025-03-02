18524049af8eb64c615fd8d3f13cf947ae869b9c
Avokadiy <braaedy1@mail.ru>
2025-03-02 09:04:05 +0500

    *footer, header

--- src/components/Footer/footer.tsx
+++ src/components/Footer/footer.tsx
@@ -1,7 +1,11 @@
+import style from "./footer.module.css"
+
 export default function Footer() {
+
     return (
-        <div>
-            
+        <div className={style.container}>
+            <p>All data in this application and its API are sourced from the official Jelly Belly website. None of the content here is claimed as original; it's all credited to and derived from Jelly Belly's official resources.</p>
+            <p>Nikita Demidov {String.fromCharCode(169)} {new Date().getFullYear()}</p>
         </div>
     )
 }
