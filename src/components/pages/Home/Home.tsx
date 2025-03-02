471ec1d3bc054093a24f7a336f7e333faaf9b2f7
Avokadiy <braaedy1@mail.ru>
2025-03-02 10:13:45 +0500

    *bug fix

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,9 +1,14 @@
-
+import style from './home.module.css'
+import JellyBellyMainPageLogo from '../../../assets/gif/JellyBellyMainPageLogo.gif'
 
 export default function Home() {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className={style.container}>
+            <img src={JellyBellyMainPageLogo} alt="Jelly Belly Logo" />
+            <div>
+                <h1>Welcome to the World of Jelly Belly:</h1>
+                <h2>A rainbow of Flavors Awaits</h2>
+            </div>
         </div>
     )
 }
